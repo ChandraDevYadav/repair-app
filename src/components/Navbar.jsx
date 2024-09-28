38a08601593079d0eaee94ac89bdf5f8eb48aceb
Chandra Dev <yadavchandradev2000@gmail.com>
2024-09-28 18:11:44 +0545

    navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -88,7 +88,7 @@ const Navbar = () => {
         </div>
 
         {/* Toggle button for mobile view */}
-        <div className="md:hidden ml-8">
+        <div className="md:hidden">
   <button onClick={toggleMenu} className="text-sm font-semibold">
     {isOpen ? (
       <img src="./close.png" alt="Close Menu" className="w-6 h-6" />
